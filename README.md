f291504b0ab8e970f6b8dc39615800fefccf9201
okothhillary <hobboltport@gmail.com>
2025-05-15 22:37:59 +0300

    swagger link

---
+++ README.md
@@ -0,0 +1,5 @@
+##
+
+Web Services; learning about APIs
+
+Link: https://project01-s3yh.onrender.com/api-docs/
--- contacts/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Hello from routes!');
-});
-
-module.exports = router;
