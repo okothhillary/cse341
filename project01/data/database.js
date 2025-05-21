c020e9f5e19a17c0d527015eb373c048a6ecd359
okothhillary <hobboltport@gmail.com>
2025-05-21 17:28:02 +0300

    steps to help me remember establishing a connection

--- project01/data/database.js
+++ project01/data/database.js
@@ -1,19 +1,22 @@
-const dotenv = require('dotenv');
-dotenv.config();
+//1. Module imports and configuration.
+const dotenv = require('dotenv');// //importing dotenv to load environment variables from a .env file
+dotenv.config();// //load environment variables from .env file
+//dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
+const MongoClient = require('mongodb').MongoClient;//importing MongoClient from mongodb package to connect to MongoDB database
 
-const MongoClient = require('mongodb').MongoClient;
+//2. Database connection handling.
+let database; //private variable to store the database client instance once connected.
 
-let database;
-
-const initDb = (callback) => {
+//3. Initialise the database connection.
+const initDb = (callback) => { // function to initialise the database connection
     if (database) {
-        console.log('Database is already initialized!');
+        console.log('Database is already initialised!');
         return callback(null, database);
     }
-    MongoClient.connect(process.env.MONGODB_URL)
-        .then((client) => {
-            database = client;
-            callback(null, database);
+    MongoClient.connect(process.env.MONGODB_URL)//if not connected, connect to the database using the connection string from the environment variables.
+        .then((client) => {//create a new MongoClient instance and connect to the database.
+            database = client; //store the connected client instance in the database variable.
+            callback(null, database);//
         })
         .catch((err) => {
             console.error('Failed to connect to the database:', err);
@@ -21,14 +24,15 @@ const initDb = (callback) => {
         });
 };
 
+//4. Accessing the database instance.
 const getDatabase = () => {
     if (!database) {
-        throw new Error('Database is not initialized!');
+        throw new Error('Database is not initialised!');
     }
         return database;
 }
 
-module.exports = {
-    initDb,
+module.exports = {//5. exporting the functions to be used in other modules.
+    initDb, 
     getDatabase,
 };
